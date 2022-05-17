33e1e17b7b4ea5ade749e967b79003efa53d1227
Gilson <ctw00849@criticaltechworks.com>
2022-05-17 15:40:34 +0100

    adding authors

---
+++ src/test/java/com/example/quarkus/author/AuthorResourceIT.java
@@ -0,0 +1,9 @@
+package com.example.quarkus.author;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class AuthorResourceIT extends AuthorResourceTest {
+
+    // Execute the same tests but in native mode.
+}
---
+++ src/test/java/com/example/quarkus/author/AuthorResourceTest.java
@@ -0,0 +1,8 @@
+package com.example.quarkus.author;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+public class AuthorResourceTest {
+
+}
